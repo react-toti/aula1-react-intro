0af0119b8f21002fe3f5be0c93c596150dab2027
Jesus Pasillas <72283720+jpasi@users.noreply.github.com>
2021-06-16 19:10:53 -0300

    Jesus

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,24 +5,35 @@ class Form extends React.Component{
     super(props);
     this.state={
       task: '',
+      priority: 'alta',
     }
     this.onInputChange = this.onInputChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   onInputChange(event){
     this.setState({task: event.target.value});
   }
 
+  handleChange(event) {
+    this.setState({priority: event.target.value});
+  }
+
   render(){
     return(
-      <form onSubmit={(e) => this.props.addTodo(e, this.state.task)}>
+      <form onSubmit={(e) => this.props.addTodo(e, this.state.task, this.state.priority)}>
         <input 
         type="text"
         value={this.state.task}
         onChange={this.onInputChange}
         placeholder="Adicione uma tarefa"
       />
-        <button type="submit">+</button>
+        <select value={this.state.priority} onChange={this.handleChange}>
+          <option priority="alta">Alta</option>
+          <option priority="media">Media</option>
+          <option priority="baixa">Baixa</option>
+        </select>
+        <button type="submit">+</button><br></br>
       </form>
     )
   }
